de7cfe02dd63b632caf43a067e0b1c5ac652a421
james sewell <jamesjosephsewell@gmail.com>
2021-03-07 20:51:44 -0600

    adds nav menu and dynamic rendering of posts

--- header.php
+++ header.php
@@ -4,4 +4,13 @@
         <?php wp_head();?>
     </head>
     <body <?php body_class();?>>
-    <header class="sticky-top"></header>
+    <header class="sticky-top">
+        <div class="container">
+        <?php wp_nav_menu(
+            array(
+                'theme_location' => 'top-menu',
+                'menu_class' => 'navigation'
+            )
+        );?>
+        </div>
+    </header>
