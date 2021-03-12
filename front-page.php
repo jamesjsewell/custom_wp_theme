9bdd65018d15f3a37dc1d67d99f1e9cbf5035d53
james sewell <jamesjosephsewell@gmail.com>
2021-03-12 02:43:50 -0600

    updates readme

--- front-page.php
+++ front-page.php
@@ -1,8 +1,10 @@
-<?php get_header();?>
+<?php get_header(); ?>
 <div class="container pt-5 pb-5">
-    <h1><?php the_title();?></h1>
-    <?php if (have_posts()) : while(have_posts()) : the_post();?>
-        <?php the_content();?>
-    <?php endwhile; endif;?>
+    <h1><?php the_title(); ?></h1>
+    Welcome, view your <a href="<?php echo site_url('/projects'); ?>">projects</a> and <a href="<?php echo site_url('/tasks'); ?>">tasks</a>. Projects and tasks are created in the wordpress dashboard and managed with the advanced custom fields plugin.
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php the_content(); ?>
+    <?php endwhile;
+    endif; ?>
 </div>
-<?php get_footer();?>
+<?php get_footer(); ?>
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+example hosted here: http://23.96.29.152/
+
 using mix to compile sass. if there are issues with scss image urls, may need to use `mix.options({processCssUrls: false})` in the webpack.mix.js file
 
 `npx mix --production` to make a prod css/js build
